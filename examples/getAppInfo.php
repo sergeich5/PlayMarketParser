422f1f32b7f26142bf123d1172bbd0722dc1cf6b
sergeich5w <75609315+sergeich5w@users.noreply.github.com>
2021-05-07 15:31:53 +0300

    Update getAppInfo.php

--- examples/getAppInfo.php
+++ examples/getAppInfo.php
@@ -3,4 +3,17 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $parser = new \Sergeich5\PlayMarketParser\PlayMarketParser();
-var_dump($parser->getAppInfo('com.zhiliaoapp.musically'));
+
+try {
+    $app = $parser->getAppInfo('com.zhiliaoapp.musically');
+
+    echo sprintf('App: %s', $app->title) . PHP_EOL;
+    echo sprintf('BundleID: %s', $app->packageName) . PHP_EOL;
+    echo sprintf('Rating: %f', $app->rating) . PHP_EOL;
+    echo sprintf('Pic: %s', $app->pic) . PHP_EOL;
+} catch (\Sergeich5\PlayMarketParser\Exceptions\ParseException $e) {
+    echo 'Unable to parse ' . $e->getMessage() . PHP_EOL;
+} catch (Exception $e) {
+    echo 'Unable to load' . PHP_EOL;
+    echo $e->getMessage() . PHP_EOL;
+}
